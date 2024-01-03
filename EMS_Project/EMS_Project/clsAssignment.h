bc1f201dd58ddfa0ac312ac974e7ee97b9c1f672
Ayman Ibrahim <abm.ayman5@gmail.com>
2024-01-03 14:35:50 +0300

    create Assignment class and its Requirements

---
+++ EMS_Project/EMS_Project/clsAssignment.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+class clsAssignment
+{
+
+
+	// Each assignment belong to 1 course. Students can submit solutions for assignments.
+	// Doctors can mark assignments, set/update grade, see stats of these grades.
+	// Assignments are solved individually.\
+
+
+};
+
