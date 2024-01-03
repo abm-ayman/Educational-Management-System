6a50c6c7fa07a78e7ca70de1c920cb4c3e124e66
Ayman Ibrahim <abm.ayman5@gmail.com>
2024-01-03 14:22:44 +0300

    create Student class and its Requirements

---
+++ EMS_Project/EMS_Project/clsStudent.h
@@ -0,0 +1,74 @@
+#pragma once
+
+
+
+
+
+class clsStudent
+{
+
+private:
+
+
+
+public:
+
+	// Student Main Menu
+	/* 
+	
+	Student main menu Should allow him to:
+		Register in course
+		List my courses
+		View a course 
+		Grades Report
+		Log out
+	
+	*/
+
+
+	// Register in course
+	/*  
+		Register in course method    >> When student choose Register in course.
+
+			he got list of courses where he is not registered:
+				ask user to register in course
+	*/
+
+
+	// List my courses method
+	
+
+	// View a course
+	/*
+		View a course mothod	>> When student choose View a course.
+
+			he got a course summary for him:
+				Course name , code
+				List Assignments report (see which submitted, which not, any grading)
+
+
+			A menu allows him to: 
+				Submit assignment solution
+				Unregister from course
+
+
+	*/
+
+
+	// Grade report method
+	/* 
+	
+		Grade report method,  >> When Student choose Grades Report:
+
+			He sees list of course, with # assignments of each course and the total grades
+			E.G. Course code, Total 4 assignments, Grade 101/200
+	
+	
+	
+	*/
+
+
+
+
+};
+
