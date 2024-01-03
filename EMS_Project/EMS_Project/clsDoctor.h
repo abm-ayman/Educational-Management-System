b56f24db7c94edd999251b06201ba560f90eaba3
Ayman Ibrahim <abm.ayman5@gmail.com>
2024-01-03 15:05:38 +0300

    create Doctor class and its Requirements

---
+++ EMS_Project/EMS_Project/clsDoctor.h
@@ -0,0 +1,65 @@
+#pragma once
+class clsDoctor
+{
+
+
+
+
+
+
+
+	// Requirements
+	/*
+
+		Each course is created b a doctor. He can add some TAs for the course.
+
+		Course has name, code, and registered students.
+
+		Doctors can create assignmentst, view them, set grades and statistics.
+
+		A Doctor main menu should be:
+			List Courses
+			Create Course
+			View Course
+			Log out >>
+				Log out option back us to the main menu (sign in / sing p menu)
+
+		If doctor selects View Course, his menu may be like:
+			List Assignments
+			Create Assignment
+			View Assignment
+			Back >> Doctor Main menu
+
+		An Assignment View menu like:
+			Show info
+			Show grades Report
+			List Solutions
+			View Solution
+			Back >> View Course
+
+		An Assignment Solution menu like:
+		Show info
+		Set grade
+		Set a comment
+		Back >> View Assignment
+
+
+
+	*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
