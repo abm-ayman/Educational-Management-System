9a7f3df2a23e46b15b781fad1ab567e39b77b333
Ayman Ibrahim <abm.ayman5@gmail.com>
2024-01-05 10:12:23 +0300

    edit .gitignore file

--- EMS_Project/EMS_Project/EMS_Project.cpp
+++ EMS_Project/EMS_Project/EMS_Project.cpp
@@ -6,3 +6,27 @@ int main()
 {
 
 }
+
+
+// System Flow
+/*
+
+	 When user access system he see some menu options (E.g. Sign In, Sign Up)
+		User can be Doctor, TA or Student
+
+	 Once logged in, user see menu options customized for him (see previous sections)
+	 Once logged out, user can go back again to main menu to sign in or signup
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
